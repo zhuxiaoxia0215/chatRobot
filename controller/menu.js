5f4953519050be5af102f0b9002a485701a6715c
zhuxiaoxia <zhuxiaoxia0215@qq.com>
2024-03-04 21:32:14 +0800

    自定义创建菜单

---
+++ controller/menu.js
@@ -0,0 +1,57 @@
+const axios = require('axios');
+const {getAccessToken} = require('./accessToken');
+const menu = {
+    button: [
+        {
+            name: '模式切换',
+            sub_button: [
+                {
+                    type: 'click',
+                    name: 'chatgpt',
+                    key: 'model_chatgpt'
+                },
+                {
+                    type: 'click',
+                    name: '人工服务',
+                    key: 'model_human'
+                },
+                {
+                    type: 'click',
+                    name: '模式查询',
+                    key: 'model_search'
+                }
+            ]
+        },
+        {
+            name: '账户',
+            sub_button: [
+                {
+                    type: 'click',
+                    name: '账户充值',
+                    key: 'balance_recharge'
+                },
+                {
+                    type: 'click',
+                    name: '余额查询',
+                    key: 'balance_search'
+                }
+            ]
+        }
+    ]
+}
+
+async function createMenu(req, res, next,) {
+    const token = await getAccessToken()
+    const url = `https://api.weixin.qq.com/cgi-bin/menu/create?access_token=${token}`;
+    axios.post(url, menu);
+    res.send('菜单创建成功');
+}
+
+function removeMenu() {
+
+}
+
+module.exports = {
+    createMenu,
+    removeMenu
+}
--- routes/chatrobot.js
+++ routes/chatrobot.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 const checkSignature = require('../controller/checkSignature');
 const message = require('../controller/message');
+const menuController = require("../controller/menu")
 
 //验证消息来源于微信服务器
 router.get('/', checkSignature);
 router.post('/', message);
+
+router.get('/menu', menuController.createMenu);
 
 module.exports = router;
